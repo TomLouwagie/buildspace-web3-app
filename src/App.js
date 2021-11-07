2ac4957f9730a42e7104a7ab08d4ff48e0b6e21f
Tom Louwagie <36488178+TomLouwagie@users.noreply.github.com>
2021-11-07 10:00:57 +0000

    Created basic UI for the app in React

--- src/App.js
+++ src/App.js
@@ -1,28 +1,126 @@
-import twitterLogo from './assets/twitter-logo.svg';
+// Import the useEffect and useState hooks!
+import { useEffect, useState } from 'react';
 import './App.css';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const TEST_GIFS = [
+	'https://media.giphy.com/media/Cwl1WYPYSmIus/giphy.gif',
+	'https://media.giphy.com/media/lPG2GDoq8demMUntts/giphy.gif'
+]
 
 const App = () => {
+  // State
+  const [walletAddress, setWalletAddress] = useState(null);
+  const [inputValue, setInputValue] = useState('');
+  const [gifList, setGifList] = useState([]);
+
+  // Actions
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log('Phantom wallet found!');
+
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log(
+            'Connected with Public Key:',
+            response.publicKey.toString()
+          );
+          setWalletAddress(response.publicKey.toString());
+        }
+      } else {
+        alert('Solana object not found! Get a Phantom Wallet 👻');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      console.log('Connected with Public Key:', response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  const sendGif = async () => {
+    if (inputValue.length > 0) {
+      console.log('Gif link:', inputValue);
+    } else {
+      console.log('Empty input. Try again.');
+    }
+  };
+
+  const onInputChange = (event) => {
+    const { value } = event.target;
+    setInputValue(value);
+  };
+
+  // If wallet not connected then render button to connect wallet
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+
+  // If wallet connected then render GIFs
+  const renderConnectedContainer = () => (
+    <div className="connected-container">
+    <input
+      type="text"
+      placeholder="Enter gif link!"
+      value={inputValue}
+      onChange={onInputChange}
+    />
+    <button className="cta-button submit-gif-button" onClick={sendGif}>
+      Submit
+    </button>
+    <div className="gif-grid">
+      {/* Map through gifList instead of TEST_GIFS */}
+      {gifList.map((gif) => (
+        <div className="gif-item" key={gif}>
+          <img src={gif} alt={gif} />
+        </div>
+      ))} 
+    </div>
+  </div>
+);
+
+  // UseEffects
+  useEffect(() => {
+    window.addEventListener('load', async (event) => {
+      await checkIfWalletIsConnected();
+    });
+  }, []);
+
+  useEffect(() => {
+    if (walletAddress) {
+      console.log('Fetching GIF list...');
+      
+      // Call Solana program here.
+
+      // Set state
+      setGifList(TEST_GIFS);
+    }
+  }, [walletAddress]);
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header">Tom's Replit website</p>
+          <p className="header">Pigeon GIF page</p>
           <p className="sub-text">
-            Wow, we are building a web3 app! ✨
+            View your pigeons in the metaverse ✨
           </p>
-        </div>
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          {!walletAddress && renderNotConnectedContainer()}
+          {walletAddress && renderConnectedContainer()}
         </div>
       </div>
     </div>
